a580bc66edd4fc916c7279285dbbfbc175d7a20b
Herick Alves <herickminecraftano@gmail.com>
2026-01-29 21:10:18 -0300

    criar interface

--- avançado/pandas/relatório_chromebook.py
+++ avançado/pandas/relatório_chromebook.py
@@ -1,6 +1,17 @@
-import pandas as pd
+#### NÃO TENHO ACESSO A API ENTÃO PRECISO CRIAR UM SISTEMA DRAG & DROP DE CSV PARA FAZER ESSE SISTEMA
 
-planilha = pd.read_excel("https://seducgogov-my.sharepoint.com/:x:/g/personal/herick_alves_seduc_go_gov_br/IQBsKuQp4xzzRbqjDcicCPuSARdwkE-PWWqTIy6la0Dsaqo?e=KLGD8X")
-planilha.raise_for_status()
-planilha.info()
-planilha.head()
+
+
+import pandas as pd
+class Geral:
+    def __init__(self):
+        self.link_planilha = f"C:/Users/heric/Downloads/controle de chromebook para garantia(CHROMEBOOK - SEDUC).csv"
+        self.load_planilha()
+        pass
+
+    def load_planilha(self):
+        self.planilha = pd.read_csv(self.link_planilha, encoding='latin1')
+        print(self.planilha.head())
+
+if __name__ == "__main__":
+    Geral()
