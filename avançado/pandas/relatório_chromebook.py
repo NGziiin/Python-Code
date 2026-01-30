49cd5c1aeae2bf06e689a9f8f9eadfc35b04156a
Herick Alves <herickminecraftano@gmail.com>
2026-01-30 00:06:26 -0300

    a

--- avançado/pandas/relatório_chromebook.py
+++ avançado/pandas/relatório_chromebook.py
@@ -1,8 +1,18 @@
 #### NÃO TENHO ACESSO A API ENTÃO PRECISO CRIAR UM SISTEMA DRAG & DROP DE CSV PARA FAZER ESSE SISTEMA
-
-
-
+import customtkinter as ctk
 import pandas as pd
+
+class Interface:
+    def __init__(self):
+        self.janela = ctk.CTk()
+        self.config_interface()
+        self.janela.mainloop()
+
+    def config_interface(self):
+        self.janela.resizable(False, False)
+        self.janela.geometry("430x390")
+        self.janela.title('Contador de chromebook')
+
 class Geral:
     def __init__(self):
         self.link_planilha = f"C:/Users/heric/Downloads/controle de chromebook para garantia(CHROMEBOOK - SEDUC).csv"
@@ -14,4 +24,4 @@ class Geral:
         print(self.planilha.head())
 
 if __name__ == "__main__":
-    Geral()
+    Interface()
